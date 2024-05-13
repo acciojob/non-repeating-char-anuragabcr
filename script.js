9bbcbd08bad699d7491dc443e3d7323a6feda923
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 11:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length; i++) {
+		sub = str.slice(i, str.length)
+		if(sub.includes(str[i])) {
+			return str[i]
+		}
+	}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
