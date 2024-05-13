2fa4351df18733664f0726fb0349e375033255bc
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 11:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	for(let i=0; i<str.length; i++) {
-		sub = str.slice(i, str.length)
-		if(sub.includes(str[i])) {
+		sub = str.slice(0, i) + str.slice(i+1, str.length)
+		if(!sub.includes(str[i])) {
 			return str[i]
 		}
 	}
 	return null
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
